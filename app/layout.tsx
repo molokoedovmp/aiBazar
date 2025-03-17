6b66b729e37c5b113332b0748b887ef0bf1ce675
Molokoedov Mikhail <molokoedovmp@gmail.com>
2025-03-17 15:14:34 +0500

    yandex metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,30 @@ export default function RootLayout({
           </div>
         </noscript>
         {/* /Yandex.Metrika counter */}
+        
+        {/* Top.Mail.Ru counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _tmr = window._tmr || (window._tmr = []);
+              _tmr.push({id: "3625705", type: "pageView", start: (new Date()).getTime()});
+              (function (d, w, id) {
+                if (d.getElementById(id)) return;
+                var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+                ts.src = "https://top-fwz1.mail.ru/js/code.js";
+                var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+                if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+              })(document, window, "tmr-code");
+            `
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://top-fwz1.mail.ru/counter?id=3625705;js=na" style={{ position: 'absolute', left: '-9999px' }} alt="Top.Mail.Ru" />
+          </div>
+        </noscript>
+        {/* /Top.Mail.Ru counter */}
       </head>
       <body className={`bg-gray-100 ${inter.className}`}>
         <EdgeStoreProvider>
