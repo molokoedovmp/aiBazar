2fbcff123ea03ca0235aff0e1e9010f933114425
Molokoedov Mikhail <molokoedovmp@gmail.com>
2025-03-17 12:28:13 +0500

    yandex metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <meta name="yandex-verification" content="31f9fbf9bddca189" />
-      <body className="bg-gray-100">
+      <head>
+        <meta name="yandex-verification" content="31f9fbf9bddca189" />
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100407501, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+              });
+            `
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/100407501" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+      </head>
+      <body className={`bg-gray-100 ${inter.className}`}>
         <EdgeStoreProvider>
           <ConvexClientProvider>
             <ThemeProvider
